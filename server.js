6fcf2aba9a0ac8cd5b19bf828f80cf7c67aed0b6
Martin Samami <martin@digitalkookie.io>
2016-10-06 00:14:44 +0200

    Remove crap, document and add notes to stuff

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ var WebSocket = require('ws');
 var WebSocketJSONStream = require('websocket-json-stream');
 var ShareDB = require('sharedb');
 var ShareDBMongo = require('sharedb-mongo');
-var ShareDBMongo = require('sharedb-mongo');
 var ShareDBRedisPubSub = require('sharedb-redis-pubsub')
-var ShareDBLogger = require('sharedb-logger')
 var richText = require('rich-text');
 
 // Set rich text as OT-type
@@ -16,26 +14,42 @@ ShareDB.types.register(richText.type);
 
 // Setup mongo database
 var db = ShareDBMongo(process.env.MONGO_URL, {safe: true});
+var collection_name = 'examples'
 
-// Setup pubsub using Redis
+// Setup redis pubsub
 var pubsub = ShareDBRedisPubSub(process.env.REDIS_URL)
 
 // Setup ShareDB backend
 var backend = new ShareDB({db: db, pubsub: pubsub});
 
-// Setup logger
-var logger = new ShareDBLogger(backend);
+// Add logging
+require('sharedb-logger')(backend)
 
-// Setup in-memory database
-// var backend = new ShareDB();
+// Add access control
+require('sharedb-access')(backend)
+
+//
+
+// Setup document access policies
+backend.allowRead(collection_name, function(docId, doc, session){
+  return true;
+});
+
+backend.allowUpdate(collection_name, function(docId, doc, session){
+  return true;
+});
 
 // Start
 createDoc(startServer);
 
-// Create initial document then fire callback
+// # Create initial document then fire callback
+//
+// We don't really want to do this later. Documents expect to be already
+// created in Rails app. We just need to verify that the requested document
+// exists at all.
 function createDoc(callback) {
   var connection = backend.connect();
-  var doc = connection.get('examples', 'richtext');
+  var doc = connection.get(collection_name, 'richtext');
   doc.fetch(function(err) {
     if (err) throw err;
     if (doc.type === null) {
@@ -47,9 +61,24 @@ function createDoc(callback) {
 }
 
 function startServer() {
-  // Create a web server that listens to WebSocket connections
-  var app = express();
-  var server = http.createServer(app);
+  /*
+    Look at using express:
+    - express
+    - express-jwt (verify access to docId)
+    - express-ws (for websocket routes)
+
+    app.ws('/editor', function(ws, req) {
+      var stream = new WebSocketJSONStream(ws);
+      backend.listen(stream);
+    });
+
+    Notes:
+    - Might also want to use `sharedb-agent-req` to pass on client name from jwt.
+    - We probably want to re-authorize a user every now and then, close connection?
+  */
+
+  // Setup http server
+  var server = http.createServer();
 
   // Connect any incoming WebSocket connection to ShareDB
   var wss = new WebSocket.Server({server: server});
